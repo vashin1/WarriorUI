d49d3b2bb86598f7bcb6913fb2a4211110ad3303
Marelle <danvdh@gmail.com>
2018-01-24 01:35:46 -0500

    WTF: Add Lagcast config

---
+++ WTF/Account/tfury/SavedVariables/LagCast.lua
@@ -0,0 +1,11 @@
+
+LagCastAccount = {
+	["barDims"] = {
+		["h"] = 18.00000068387239,
+		["w"] = 195.0000004787107,
+	},
+	["disableTimerDisplay"] = false,
+	["disableSpamPrevention"] = false,
+	["bufferMs"] = 15,
+	["minLag"] = 0,
+}
